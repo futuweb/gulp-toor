ae070dba9b78945dee1e8fea3d7c4140fb423e6c
TooBug <i@toobug.net>
2014-12-26 12:30:31 +0800

    fix log

--- index.js
+++ index.js
@@ -8,27 +8,29 @@ var fs = require('fs');
 module.exports = function (options) {
 	var oldOut = options.out;
 	return through.obj(function (file, enc, cb) {
+		gutil.log('optimizing:',file.path);
 		if (file.isNull()) {
 			this.push(file);
 			return cb();
 		}
-
 		if (file.isStream()) {
 			this.emit('error', new gutil.PluginError('toor', 'Streaming not supported'));
 			return cb();
 		}
+		var self = this;
 		options.name = path.join(path.dirname(path.relative(options.baseUrl,file.path)),path.basename(file.path,'.js'));
-		options.out1 = path.join(oldOut,path.basename(file.path));
 		options.out = path.join(oldOut,path.dirname(options.name),path.basename(file.path));
 		rjs.optimize(options, function () {
+			gutil.log('finished');
 			file.contents = fs.readFileSync(options.out);
 			options.out = oldOut;
-			this.push(file);
+			self.push(file);
 			cb();
 		}, function(err) {
-			this.emit('error', new gutil.PluginError('toor', 'r.js optimize failed.'));
+			self.emit('error', new gutil.PluginError('toor', err), {showStack: true});
 			options.out = oldOut;
 			cb();
 		});
 	});
 };
+
