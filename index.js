a559817919d5449de9e90face3c4c4300fba4a0c
TooBug <i@toobug.net>
2015-09-21 11:40:19 +0800

    cache特性

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ var through = require('through2');
 var rjs = require('requirejs');
 var path = require('path');
 var fs = require('fs');
+var isDebug = false;
+
+var log = function(){
+	if(isDebug){
+		console.log.apply(console,arguments);
+	}
+};
 
 // save dependencies tree for next time
 var saveDependencies = function(dest, cachePath){
 	var madge = require('madge');
 
 	var dependencyObject = madge(dest,{
-		requireConfig:'./web/config.js',
+		// requireConfig:'./web/config.js',
 		format:'amd',
 		findNestedDependencies:true
 	});
@@ -24,6 +31,8 @@ var saveDependencies = function(dest, cachePath){
 		path.join(cachePath, 'dependencies.json'),
 		JSON.stringify(dependencyObject.tree)
 	);
+
+	log('saveDependencies success');
 
 };
 
@@ -38,10 +47,12 @@ var saveModifyMap = function(cachePath, modifyMap){
 		JSON.stringify(modifyMap)
 	);
 
+	log('saveModifyMap success');
 };
 
 module.exports = function (options) {
-	var requireConfig = options.requireConfig || {};
+	// 向下兼容
+	var requireConfig = options.requireConfig || options || {};
 	if(!options.cache) options.cache = {};
 
 	// 所有依赖列表
@@ -50,6 +61,8 @@ module.exports = function (options) {
 	var fileModifyMap = {};
 	// 当前扫描时用的文件修改时间缓存
 	var fileModifyCache = {};
+
+	isDebug = options.cache.debug;
 
 	// 如果使用缓存，则更新依赖列表和文件上次修改时间列表
 	if(options.cache.enable){
@@ -70,34 +83,41 @@ module.exports = function (options) {
 		var modelPath = path.join(path.dirname(path.relative(requireConfig.baseUrl,file.path)),path.basename(file.path,'.js'));
 		if(options.cache.enable){
 			var isModified = false;
-			var modelDenpendencies = dependencies[modelPath];
-			modelDenpendencies.unshift(modelPath);
+			log('modelPath',modelPath);
+			var modelDependencies = dependencies[modelPath];
+			// console.log('modelDependencies',modelDependencies);
+			if(!modelDependencies){
+				isModified = true;
+			}else{
+				modelDependencies.unshift(modelPath);
 
-			modelDenpendencies.forEach(function(dependency){
-				var filePath = path.join(requireConfig.baseUrl,dependency + '.js');
-				// 路径不存在
-				if(!fs.existsSync(filePath)){
-					// console.log(filePath,'not exist');
-					return;
-				}
-				// 获取当前文件修改时间
-				var modifyTime = fileModifyCache[filePath];
-				if(!modifyTime){
-					var stat = fs.statSync(filePath);
-					modifyTime = fileModifyCache[filePath] = +stat.mtime;
-				}
-				// 之前没有保存过这个时间
-				if(!fileModifyMap[filePath]){
-					// console.log(filePath,'no time');
-					isModified = true;
-					return;
-				}
-				if(modifyTime > fileModifyMap[filePath]){
-					console.log(filePath,'is newer');
-					isModified = true;
-				}
-			});
-			console.log('isModified',isModified);
+				modelDependencies.forEach(function(dependency){
+					var filePath = path.join(requireConfig.baseUrl,dependency + '.js');
+					// 路径不存在
+					if(!fs.existsSync(filePath)){
+						log(filePath,'not exist');
+						return;
+					}
+					// 获取当前文件修改时间
+					var modifyTime = fileModifyCache[filePath];
+					if(!modifyTime){
+						log(filePath,'no time cache');
+						var stat = fs.statSync(filePath);
+						modifyTime = fileModifyCache[filePath] = +stat.mtime;
+					}
+					// 之前没有保存过这个时间
+					if(!fileModifyMap[filePath]){
+						log(filePath,'no time');
+						isModified = true;
+						return;
+					}
+					if(modifyTime > fileModifyMap[filePath]){
+						log(filePath,'is newer');
+						isModified = true;
+					}
+				});
+			}
+			log('isModified',isModified);
 			if(!isModified){
 				return cb(null);
 			}
@@ -112,6 +132,8 @@ module.exports = function (options) {
 		}
 		var self = this;
 		requireConfig.name = path.join(path.dirname(path.relative(requireConfig.baseUrl,file.path)),path.basename(file.path,'.js'));
+		// 兼容windows
+		requireConfig.name = requireConfig.name.replace(/\\/g,'/');
 		requireConfig.out = function(text,sourceMapText){
 			file.contents = new Buffer(text);
 			// todo:sourcemap
@@ -126,7 +148,7 @@ module.exports = function (options) {
 		});
 	},function(){
 		if(options.cache.enable){
-			console.log('saving dependencies...');
+			log('saving dependencies...');
 			var cachePath = options.cache.cachePath || './.gulp-toor-cache';
 			saveDependencies(options.cache.dest, cachePath);
 			saveModifyMap(cachePath, fileModifyCache);
